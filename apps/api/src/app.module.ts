f9d3e5e9109cc1ab5f7d84dca84cf7fb326833ac
aluxey <aurelien.luxeyy@gmail.com>
2026-04-07 19:58:53 +0200

    feat(api): add backend domain modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { AdminModule } from './modules/admin/admin.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ContentModule } from './modules/content/content.module';
+import { GamesModule } from './modules/games/games.module';
 import { HealthModule } from './modules/health/health.module';
+import { ProgressModule } from './modules/progress/progress.module';
+import { QuizModule } from './modules/quiz/quiz.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    HealthModule,
+    AuthModule,
+    UsersModule,
+    GamesModule,
+    ContentModule,
+    QuizModule,
+    ProgressModule,
+    AdminModule,
+  ],
 })
 export class AppModule {}
